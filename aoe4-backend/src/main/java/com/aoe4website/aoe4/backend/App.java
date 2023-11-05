e17d97c0c99fdd6a2d886f05d62f4d16b377e323
Xiaolequan <stevenli2088@hotmail.com>
2023-11-05 15:50:33 -0800

    Setup backend an frontend enviornment

---
+++ aoe4-backend/src/main/java/com/aoe4website/aoe4/backend/App.java
@@ -0,0 +1,13 @@
+package com.aoe4website.aoe4.backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
